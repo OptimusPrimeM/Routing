175de65da74984118dc6a6ba47355461234e6d69
Menik <menikdisney@gmail.com>
2019-02-01 14:56:15 +0600

    Adding and receivieng query parameters and combining multiple observables

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,9 @@
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/observable/combineLatest';
+
 
 @Component({
   selector: 'github-followers',
@@ -9,10 +13,22 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(private service: GithubFollowersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+      .subscribe(combine => {
+        let id = combine[0].get('id');
+        let page = combine[1].get('page');
+      });
+
+    // this.service.getAll({ id: id, page: page });
+
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
-}
+} 
